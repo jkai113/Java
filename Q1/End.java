5a864b5670250384db6ca0dbb426bd305fb10f2b
FELIPE LACERDA <10671086960@educacional.up>
2022-06-14 09:06:03 -0300

    CU

--- Q1/End.java
+++ Q1/End.java
@@ -2,6 +2,21 @@ package Q1;
 
 
 public class End {
+    private  String Logradouro;
+    private int numero;
+    private String Complemento;
+    private String Bairro;
+    private String CEP;
+    private String Cidade;
+
+    public End(String Logradouro, int numero, String Complemento, String Bairro, String CEP, String Cidade){
+        this.Logradouro = Logradouro;
+        this.numero = numero;
+        this.Complemento = Complemento;
+        this.Bairro = Bairro;
+        this.CEP = CEP;
+        this.Cidade = Cidade;
+    }
 
     
 }
--- Q1/Main.java
+++ Q1/Main.java
@@ -1,14 +1,42 @@
 package Q1;
 
-/**
- * Main
- */
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    private  static int op;
+    static funcs p = new funcs();
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+       
+        while(true){
+            
+            System.out.println("********MENU********");
+            System.out.println("O que deseja fazer? \n 1- Criar contato. \n 2 - Remover contato. \n 3-Editar contato \n 4 - Listar em orderm alfabética.");
+            op = teclado.nextInt();
+            switch(op){
+                case 1:
+                p.CriaContato();
+                break;
+                case 2:
+                p.RemoveContato();
+                break;
+                case 3:
+                p.EditaContato();
+                break;
+                case 4:
+                p.ListaContato();
+                break;
+            }    
+        }
         
 
 
     }
+    public static void CriaContato(){
+
+    }
+    public static void RemoveContato(){
+
+    }
+
 }
